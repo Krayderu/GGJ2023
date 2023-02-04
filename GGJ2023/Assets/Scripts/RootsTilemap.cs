da58539a782c93322b0f600b1fa41851499be233
Sergenti <florian.rieder@unil.ch>
2023-02-04 11:33:03 +0100

    Added tilemap interaction

--- GGJ2023/Assets/Scripts/RootsTilemap.cs
+++ GGJ2023/Assets/Scripts/RootsTilemap.cs
@@ -5,19 +5,55 @@ using UnityEngine.Tilemaps;
 
 public class RootsTilemap : MonoBehaviour
 {
+    public Tile currentTile;
+    public Tilemap tilemap;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log(gameObject);
+        //tilemap.ClearAllTiles();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!Input.GetMouseButtonDown(0)) return;
+
+        // get the position of the tile being clicked on
+        Vector3 point = GetMouseWorldPosition();
+        //Vector3 point = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        if (point == Vector3.zero) return;
+        Debug.Log(point);
+        Vector3Int tilePos = tilemap.WorldToCell(point);
+
+        Debug.Log("PLACING TILE" + tilePos);
+        //Vector3Int pos = new Vector3Int(0,0,-20);
+        PlaceTile(currentTile, tilePos);
     }
 
     void PlaceTile(Tile tile, Vector3Int pos){
-        
+        // check if the tile is empty
+        if (!tilemap.GetTile(pos))
+        {
+            // place tile
+            tilemap.SetTile(pos, tile);
+        }
     }
+
+    public Vector3 GetMouseWorldPosition()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Plane plane = new Plane(Camera.main.transform.forward, transform.position);
+        if (plane.Raycast(ray, out float distance))
+        {
+            Vector3 hitPoint = ray.GetPoint(distance);
+            return hitPoint;
+        }
+        else
+        {
+            return Vector3.zero;
+        }
+    }
+
 }
