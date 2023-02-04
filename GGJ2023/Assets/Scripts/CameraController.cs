fdd3dd53ada86d0f774b0a7574a16ba00889e951
Florian <florian.rieder@unil.ch>
2023-02-04 15:59:33 +0100

    Added dragging limits

--- GGJ2023/Assets/Scripts/CameraController.cs
+++ GGJ2023/Assets/Scripts/CameraController.cs
@@ -6,6 +6,8 @@ public class CameraController : MonoBehaviour
 {
     [SerializeField] private int dragMargin = 10;
     [SerializeField] private float dragSpeed = 10f;
+    [SerializeField] private int maxHeight = 500;
+    [SerializeField] private int minHeight = -500;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,7 +19,7 @@ public class CameraController : MonoBehaviour
     {
 		Vector3 position = transform.position;
         Vector3 currentPanSpeed = Vector3.zero;
-        
+
         // Get mouse input and apply movement
 		if (Input.mousePosition.y >= Screen.height - dragMargin)
 		{
@@ -29,6 +31,11 @@ public class CameraController : MonoBehaviour
 		}
 
         position.y += currentPanSpeed.y * Time.deltaTime;
+
+        // Limit camera movement
+		//position.x = Mathf.Clamp(position.x, -panLimit.x, panLimit.x);
+		position.y = Mathf.Clamp(position.y, minHeight, maxHeight);
+
         transform.position = position;
     }
 }
