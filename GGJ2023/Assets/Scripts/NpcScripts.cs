3aadb70e67b544428e8f1a3b53317f0641937375
Sergenti <florian.rieder@unil.ch>
2023-02-04 23:23:54 +0100

    added silvestre

--- GGJ2023/Assets/Scripts/NpcScripts.cs
+++ GGJ2023/Assets/Scripts/NpcScripts.cs
@@ -2,31 +2,80 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+enum State {GOING, HOVER, BACK, WITHSTUFF};
+
 public class NpcScripts : MonoBehaviour
 {
     [SerializeField] private Transform[] wayPoints;
     [SerializeField] private Transform[] hoverPoints;
+    [SerializeField] private int rootsThreshold = 10;
+    [SerializeField] private int minimumRoots = 0;
 
-    public int currentWaypoint;
-    public int tilePlaced;
-
+    private int currentWaypoint = 0;
+    private int currentHoverPoint = 0;
+    private State currentState = State.GOING;
+    public int rootsPlaced = 1;
 
     
 
     private void Update()
     {
-        
-        MoveTowards(wayPoints[1]);
-        if(Vector3.Distance(transform.position, wayPoints[1].position) <= 1)
-        {
-            currentWaypoint = 1;
-            HoverAround(wayPoints[1]);
+        if(rootsPlaced < minimumRoots) return;
+
+        if (currentState == State.GOING){
+            MoveTowards(wayPoints[currentWaypoint+1]);
+            if (Vector3.Distance(transform.position, wayPoints[currentWaypoint+1].position) <= 1){
+                currentWaypoint++;
+                if (currentWaypoint == wayPoints.Length - 1){
+                    currentState = State.HOVER;
+                }
+            }
         }
-        if (currentWaypoint == 1 && tilePlaced >= 5)
-        {
-            MoveTowards(wayPoints[0]);
+        else if (currentState == State.HOVER){
+            int rootsPlaced = 10; //TODO
+            MoveTowards(hoverPoints[currentHoverPoint+1]);
+            if (Vector3.Distance(transform.position, hoverPoints[currentHoverPoint+1].position) <= 1){
+                currentHoverPoint++;
+                if (currentHoverPoint == hoverPoints.Length && Vector3.Distance(transform.position, hoverPoints[currentHoverPoint].position)<= 1){
+                    currentHoverPoint = 0;
+                    // Wait (polish)
+                    // Flip sprite
+                }
+            if (rootsPlaced >= rootsThreshold && currentHoverPoint == 0){
+                currentState = State.BACK;
+                // TODO Flip sprite horizontally
+            }
+            }
+        }
+        else if (currentState == State.BACK){
+            MoveTowards(wayPoints[currentWaypoint-1]);
+            if (Vector3.Distance(transform.position, wayPoints[currentWaypoint-1].position) <= 1){
+                currentWaypoint--;
+                if (currentWaypoint == 0){
+                    currentState = State.WITHSTUFF;
+                    // Wait (polish)
+                    // Flip sprite
+                }
+            }
+
+        }
+        else if (currentState == State.WITHSTUFF){
+            
         }
     }
+
+        
+        //MoveTowards(wayPoints[1]);
+        // if(Vector3.Distance(transform.position, wayPoints[1].position) <= 1 %% )
+        // {
+        //     currentWaypoint = 1;
+        //     HoverAround();
+        // }
+        // if (currentWaypoint == 1 && tilePlaced >= 5)
+        // {
+        //     MoveTowards(wayPoints[0]);
+        // }
+    
 
     private void MoveTowards(Transform pos)
     {
@@ -34,8 +83,8 @@ public class NpcScripts : MonoBehaviour
         transform.position = Vector3.Lerp(transform.position, destination, Time.deltaTime);
     }
 
-    private void HoverAround(Transform pos)
+    private void HoverAround()
     {
-      
+        //move in between a set number of points
     }
 }
