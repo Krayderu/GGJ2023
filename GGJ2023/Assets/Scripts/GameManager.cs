982bd6ee2dcc53b10a8a8cff523b331f290d4723
Florian <florian.rieder@unil.ch>
2023-02-05 04:05:53 +0100

    Revert "Merge branch 'main' of https://github.com/Krayderu/GGJ2023"
    This reverts commit 2fdb5aa2790f6d053d788be3134009361c7e7d1e, reversing
    changes made to d74af081bd5f4ca9fae516d25cfc184af2971d98.

--- GGJ2023/Assets/Scripts/GameManager.cs
+++ GGJ2023/Assets/Scripts/GameManager.cs
@@ -9,13 +9,16 @@ using UnityEngine;
 public class GameManager : MonoBehaviour {
 
     [SerializeField] private TileData[] tileTypes;
+    [SerializeField] private RootsTilemap rootsTilemap;
 
     public static GameManager Instance { get; private set; }
+
+    public int water = 100;
+    [SerializeField] private int rootCost = 10;
 
     private void Awake()
     { 
         // If there is an instance, and it's not me, delete myself.
-        
         if (Instance != null && Instance != this) 
         { 
             Destroy(this); 
@@ -24,12 +27,41 @@ public class GameManager : MonoBehaviour {
         {
             Instance = this; 
         }
+    }
 
-        //Debug.Log(tileTypes);
+    private void Start()
+    {
+        StartCoroutine(everySecond());
+    }
+
+    IEnumerator everySecond() {
+        while (true) {
+            DoResources();
+            yield return new WaitForSeconds(1);
+        }
     }
 
     public TileData GetNextTile(){
         int randomIndex = Random.Range(0, tileTypes.Length);
         return tileTypes[randomIndex];
+    }
+
+    private void DoResources(){
+        int wateredRoots = rootsTilemap.GetNumberOfWateredRoots();
+
+        water += wateredRoots;
+    }
+
+    public bool CanPay(){
+        if (water - rootCost < 0) return false;
+        return true;
+    }
+
+    public void Pay(){
+        if(!CanPay()) {
+            Debug.Log("Can't pay but pays anyway !!! Someone took advantage of a loophole ! BE THEY DAMNED !");
+            return;
+        }
+        water = water - rootCost;
     }
 }
