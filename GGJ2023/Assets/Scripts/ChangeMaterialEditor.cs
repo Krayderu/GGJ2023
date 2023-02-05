be5151795cfcde8794828012012c5e6b708af7b1
Sergenti <florian.rieder@unil.ch>
2023-02-05 04:39:22 +0100

    Recovered Shadowcaster Shader

---
+++ GGJ2023/Assets/Scripts/ChangeMaterialEditor.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEditor;
+
+public class ChangeMaterialEditor : EditorWindow
+{
+    public Material newMaterial;
+
+    [MenuItem("Tools/Change Material")]
+    public static void ShowWindow()
+    {
+        GetWindow<ChangeMaterialEditor>("Change Material");
+    }
+
+    private void OnGUI()
+    {
+        newMaterial = (Material)EditorGUILayout.ObjectField("New Material", newMaterial, typeof(Material), false);
+
+        if (GUILayout.Button("Change Material"))
+        {
+            SpriteRenderer[] spriteRenderers = FindObjectsOfType<SpriteRenderer>();
+            foreach (SpriteRenderer spriteRenderer in spriteRenderers)
+            {
+                Undo.RecordObject(spriteRenderer, "Change Material");
+                spriteRenderer.sharedMaterial = newMaterial;
+                spriteRenderer.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On;
+                spriteRenderer.receiveShadows = true;
+            }
+        }
+    }
+}
